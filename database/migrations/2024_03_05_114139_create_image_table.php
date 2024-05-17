f38a8fda75dab60b4373f3d01a7fee56ed808426
r0zh <r0zh017@proton.me>
2024-05-17 15:31:21 +0000

    Add 'name' column to the 'image' table

--- database/migrations/2024_03_05_114139_create_image_table.php
+++ database/migrations/2024_03_05_114139_create_image_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('image', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('style_id');
             $table->string('path');
