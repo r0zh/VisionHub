0ef683d73454b419e810b83725484a8d8ea57146
r0zh <r0zh017@proton.me>
2024-05-20 14:05:25 +0000

    Add new features and fix bugs in UploadImage component

--- app/Livewire/Images/UploadImage.php
+++ app/Livewire/Images/UploadImage.php
@@ -21,11 +21,14 @@ use Filament\Forms\Components\FileUpload;
 use Livewire\Component;
 use Illuminate\Contracts\View\View;
 
+use Illuminate\Support\Facades\Storage;
+
 class UploadImage extends Component implements HasForms
 {
     use InteractsWithForms;
 
     public ?array $data = [];
+    public bool $bool = false;
 
     public function mount(): void
     {
@@ -39,37 +42,49 @@ class UploadImage extends Component implements HasForms
                 Section::make('Upload Image')->schema([
                     Grid::make([
                         'default' => 1,
-                        'md' => 2
+                        'md'      => 2
                     ])->schema([
                                 Group::make()->schema([
-                                    TextInput::make('name')->required(),
+                                    TextInput::make('name'),
                                     TextInput::make('description')->nullable(),
                                     TextInput::make('positivePrompt')->required(),
                                     TextInput::make('negativePrompt')->required(),
-                                    TextInput::make('seed')->numeric()->required(),
-                                    
+                                    TextInput::make('seed')->numeric()->required()->maxValue(4294967296),
+
                                 ]),
 
                                 Group::make()->schema([
-                                    Select::make('style_id')->options(Style::pluck('name', 'id')->toArray())->label('Style')->required(),
-                                    Section::make('Tags')->schema([
-
-                                        Select::make('tags')
-                                            ->multiple()
-                                            ->options(Tag::pluck('name', 'id')->toArray())
-                                    ]),
+                                    Select::make('style_id')->live()->preload()->relationship(name: 'style', titleAttribute: 'name')->required(),
 
 
-                                    FileUpload::make('imagePath')->imageEditor()
+                                    Select::make('tags')
+                                        ->multiple()
+                                        ->live()
+                                        ->disabled(function (): bool {
+                                            if ($this->data['style_id']) {
+                                                return true;
+                                            } else {
+                                                return false;
+                                            };
+                                        })
+                                        ->preload()
+                                        ->relationship('tags', 'name')
+                                        ->createOptionForm([
+                                            TextInput::make('name')
+                                                ->required(),
+                                        ]),
+
+
+                                    FileUpload::make('imagePath')->imageEditor()->visibility('private')
                                         ->imageEditorAspectRatios([
                                             '16:9',
                                             '4:3',
                                             '1:1',
                                         ])->image()->label('Upload Image')->required(),
-                                        Checkbox::make('public')
+                                    Checkbox::make('public')
                                 ])
                             ]),
-                    
+
                 ])
             ])
             ->statePath('data')
@@ -78,13 +93,28 @@ class UploadImage extends Component implements HasForms
 
     public function create(): void
     {
-        $data = $this->form->getState();
+        $data            = $this->form->getState();
         $data['user_id'] = auth()->id();
-        $data['path'] = $data['imagePath'];
+        if ($data['public']) {
+            $newPath      = "images/" . auth()->id() . '_' . explode('@', auth()->user()->email)[0] . '/' . $data['imagePath'];
+            $data['path'] = $newPath;
+            Storage::disk('public')->put($newPath, Storage::disk('public')->get($data['imagePath']));
+        } else {
+            $newPath      = "private_images/" . auth()->id() . '_' . explode('@', auth()->user()->email)[0] . '/' . $data['imagePath'];
+            $data['path'] = $newPath;
+            Storage::disk('local')->put($newPath, Storage::disk('public')->get($data['imagePath']));
+        }
+
+        Storage::disk('public')->delete($data['imagePath']);
 
         $record = Image::create($data);
-
         $this->form->model($record)->saveRelationships();
+    }
+
+    public function resetName(): void
+    {
+        $this->reset('data.name');
+        $this->bool = true;
     }
 
     public function render(): View
