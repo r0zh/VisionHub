02c40d8924c6d5b9c4c27b4589c9a02051c312ce
Roberto Sánchez Martín <32245814+r0zh@users.noreply.github.com>
2024-05-23 11:26:06 +0000

    chore: Update API request throttle limit to 960 requests per minute

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            "throttle:120,1",
+            "throttle:960,1",
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
 
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
