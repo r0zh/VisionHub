68a9f31fa0cc9d4bb9e6d888976f54f879fb62e3
Roberto Sánchez Martín <32245814+r0zh@users.noreply.github.com>
2024-05-24 16:02:04 +0000

    refactor: Update GenerateImage form structure

--- app/Livewire/Images/GenerateImage.php
+++ app/Livewire/Images/GenerateImage.php
@@ -7,11 +7,22 @@ use Filament\Forms;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
+
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+
 use Livewire\Component;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Auth;
 
 class GenerateImage extends Component implements HasForms
 {
@@ -31,33 +42,54 @@ class GenerateImage extends Component implements HasForms
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('user_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('style_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('name')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('path')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('seed')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\Textarea::make('positivePrompt')
-                    ->required()
-                    ->columnSpanFull(),
-                Forms\Components\Textarea::make('negativePrompt')
-                    ->columnSpanFull(),
-                Forms\Components\Toggle::make('public')
-                    ->required(),
-                Forms\Components\TextInput::make('description')
-                    ->maxLength(255),
+                Section::make('Upload Image')->schema([
+                    Grid::make([
+                        'default' => 1,
+                        'md' => 2
+                    ])->schema([
+                                Group::make()->schema([
+                                    Select::make('style_id')->preload()->relationship(name: 'style', titleAttribute: 'name')->required(),
+                                    Select::make('checkpoint_id')->preload()->relationship(name: 'checkpoint', titleAttribute: 'name')->required(),
+                                    TextInput::make('positivePrompt')->required(),
+                                    TextInput::make('negativePrompt')->required(),
+                                    TextInput::make('seed')->numeric()->required()->maxValue(4294967296),
+                                    Select::make('loras')
+                                        ->multiple()
+                                        ->preload()
+                                        ->relationship('loras', 'name')
+                                        ->createOptionForm([
+                                            TextInput::make('name')
+                                                ->required(),
+                                            TextInput::make('fileName')
+                                                ->required(),
+                                            RichEditor::make('description')->nullable(),
+                                        ]),
+
+                                    Select::make('embeddings')
+                                        ->multiple()
+                                        ->preload()
+                                        ->relationship('embeddings', 'name')
+                                        ->createOptionForm([
+                                            TextInput::make('name')
+                                                ->required(),
+                                            TextInput::make('fileName')
+                                                ->required(),
+                                            RichEditor::make('description')->nullable(),
+                                        ]),
+
+                                ]),
+
+                                Group::make()->schema([
+                                    Select::make('style_id')->preload()->relationship(name: 'style', titleAttribute: 'name')->required(),
+                                ]),
+
+                            ]),
+                ])
             ])
             ->statePath('data')
             ->model(Image::class);
     }
+
 
     public function create(): void
     {
@@ -75,11 +107,31 @@ class GenerateImage extends Component implements HasForms
         $this->fetching = false;
     }
 
-    public function save($data): void
+    public function save($data)
     {
         $record = Image::create($data);
 
         $this->form->model($record)->saveRelationships();
+
+        $userPath = Auth::user()->id . '_' . explode('@', Auth::user()->email)[0];
+        Storage::disk('public')->move($this->imagePath, 'images/' . $userPath . '/' . $this->name . '.png');
+
+        // save the image path to the database
+        $imagePath = 'images/' . $userPath . '/' . $this->name . '.png';
+        Image::create([
+            'seed' => $this->seed,
+            'positivePrompt' => $this->positive_prompt,
+            'negativePrompt' => $this->negative_prompt,
+            'public' => true,
+            'style' => $this->style,
+            // Store the image in the public or private directory
+            'path' => $imagePath,
+            'created_at' => now(),
+            'user_id' => Auth::user()->id,
+        ]);
+
+        // redirect to gallery
+        return redirect()->to('/gallery');
     }
 
     public function render(): View
