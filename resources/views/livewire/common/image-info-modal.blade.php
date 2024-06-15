52294e25122226a3c90f21aad451d17df352c75d
r0zh <r0zh017@proton.me>
2024-06-15 15:59:14 +0000

    improve style

--- resources/views/livewire/common/image-info-modal.blade.php
+++ resources/views/livewire/common/image-info-modal.blade.php
@@ -1,12 +1,20 @@
-<x-filament::modal id="image-info-modal" width="7xl" alignment="center">
-    <x-slot name="header">Edit Image</x-slot>
+<x-filament::modal id="image-info-modal" width="7xl" alignment="center" class="dark:text-white">
+
     <div>
-        <div class="grid grid-cols-2 gap-3 p-4 rounded-xl shadow-lg w-full">
-            <div class="w-fit">
+        <div class="grid grid-cols-2 gap-3 p-4  w-full">
+            <div class="w-fit grid-column-span-2">
                 <img src="@if ($image->public == 1) {{ asset('storage/' . $image->path) }} @else {{ url($image->path) }} @endif"
-                    alt="{{ $image->positivePrompt }}" class="w-fit max-h-[80vh]" />
+                    alt="{{ $image->positivePrompt }}" class="w-fit max-h-[80vh] rounded-xl shadow-lg" />
             </div>
             <div class="p-2">
+                @if ($this->image->name)
+                    <h1 class="text-3xl font-bold">Name</h1>
+                    <p>{{ $image->name }}</p>
+                @endif
+                @if ($this->image->description)
+                    <h1 class="text-3xl font-bold">Description</h1>
+                    <p>{{ $image->description }}</p>
+                @endif
                 <h1 class="text-3xl font-bold">Seed</h1>
                 <p>{{ $image->seed }}</p>
                 <h1 class="text-3xl font-bold">Positive Prompt</h1>
@@ -29,12 +37,13 @@
     </div>
     <x-slot name="footerActions">
         @if ($image->user->id == auth()->id() || auth()->user()->hasRole('moderator') || auth()->user()->hasRole('admin'))
-            <x-filament::button wire:click='togglePublic' color="{{ $image->public ? 'danger' : 'warning' }}">
+            <x-filament::button wire:click='togglePublic' color="{{ $image->public ? 'gray' : 'info' }}"
+                icon="{{ $image->public ? 'heroicon-o-lock-closed' : 'heroicon-o-lock-open' }}">
                 {{ $image->public ? 'Make Private' : 'Make Public' }}
             </x-filament::button>
         @endif
         @if ($image->user->id == auth()->id() || auth()->user()->hasRole('admin'))
-            <x-filament::button wire:click='deleteImage' color="danger">
+            <x-filament::button wire:click='deleteImage' color="danger" icon="heroicon-o-trash">
                 Delete
             </x-filament::button>
         @endif
