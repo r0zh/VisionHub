6d4824261be011f8d4159fc98b5be65893ff8d49
r0zh <r0zh017@proton.me>
2024-06-15 16:19:56 +0000

    feat: Add lara-zeus/accordion dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './vendor/lara-zeus/accordion/resources/views/**/*.blade.php',
     ],
 
     theme: {
