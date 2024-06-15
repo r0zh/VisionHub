3860e997d746b6318df7dfd9cb1ce19cd6bf7efd
r0zh <r0zh017@proton.me>
2024-06-15 23:01:55 +0000

    refactor: Update image-component to use URL instead of asset for image source

--- resources/views/livewire/common/image-component.blade.php
+++ resources/views/livewire/common/image-component.blade.php
@@ -1,7 +1,7 @@
 <div
     class="relative py-2 px-2 rounded-md hover:bg-[#6e42c185] transition-all overflow-hidden w-[370px] h-[500px] flex items-center">
-    <img src="@if ($image->public == 1) {{ asset('storage/' . $image->path) }} @else {{ $image->path }} @endif"
+
+    <img src="@if ($image->public == 1) {{ asset('storage/' . $image->path) }} @else {{ url($image->path) }} @endif"
         alt="{{ $image->positivePrompt }}"
-        class="object-cover rounded-lg cursor-pointer overflow-hidden h-[500px] w-[570px]"
-        wire:click="imageSelected" />
+        class="object-cover rounded-lg cursor-pointer overflow-hidden h-[500px] w-[570px]" wire:click="imageSelected" />
 </div>
