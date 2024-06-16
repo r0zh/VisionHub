196ad1fc69fa704b9075f0bf3dd734932e9b741c
r0zh <r0zh017@proton.me>
2024-06-16 18:45:38 +0000

    refactor: Remove unused image alt attribute and update user link color

--- resources/views/livewire/common/image-info-modal.blade.php
+++ resources/views/livewire/common/image-info-modal.blade.php
@@ -4,9 +4,9 @@
             <div class="flex justify-center items-center">
                 <div class="w-fit h-fit">
                     <img src="@if ($image->public == 1) {{ asset('storage/' . $image->path) }} @else {{ url($image->path) }} @endif"
-                        alt="{{ $image->positivePrompt }}" class="xl:w-fit max-h-[80vh] rounded-xl shadow-lg" />
+                        class="xl:w-fit max-h-[80vh] rounded-xl shadow-lg" />
                     <h1 class="text-xl font-bold">By
-                        <a href="/user/{{ $image->user_id }}" class="focus-visible:outline-none">
+                        <a href="/user/{{ $image->user_id }}" class="focus-visible:outline-none text-yellow-200">
                             {{ $image->user->name }}
                         </a>
                     </h1>
