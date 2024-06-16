2781a49574b4555dd422dd98fc5fbed3a3859dbf
r0zh <r0zh017@proton.me>
2024-06-16 13:20:42 +0000

    refactor: make some api routes public

--- routes/api.php
+++ routes/api.php
@@ -26,17 +26,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('models/public', [ThreeDModelController::class, 'index']);
+Route::get('model/{id}/thumbnail', [ThreeDModelServeController::class, 'serveThreeDModelThumbnail'])->name('models.thumbnail');
+
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/auth/checkToken', [AuthController::class, 'checkToken']);
     Route::post('auth/logout', [AuthController::class, 'logout']);
     Route::get('images/public', [ImageController::class, 'index']);
     Route::get('images/user', [ImageController::class, 'userImages']);
     Route::get('images/all', [ImageController::class, 'allImages'])->middleware(['role:admin']);
     Route::get('images/{id}/get', [ImageServeController::class, 'serveImage'])->name('images.download');
     Route::post('models/upload', [ThreeDModelController::class, 'store']);
-    Route::get('models/public', [ThreeDModelController::class, 'index']);
-    Route::get('models/user', [ThreeDModelController::class, 'userModels']);
+    Route::get('models/user/{id}', [ThreeDModelController::class, 'userModels']);
     Route::get('model/{id}/get', [ThreeDModelServeController::class, 'serveThreeDModel'])->name('models.download');
-    Route::get('model/{id}/thumbnail', [ThreeDModelServeController::class, 'serveThreeDModelThumbnail'])->name('models.thumbnail');
     Route::get('user/{id}/get', [UserController::class, 'user'])->name('user.get');
     Route::get('user/get_id', [UserController::class, 'id'])->name('user.get_id');
 });
