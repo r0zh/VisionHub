bb0d2b5109f7827397146cb6ce762767e4662f6d
r0zh <r0zh017@proton.me>
2024-06-17 09:15:21 +0000

    chore: Add new translations for Spanish language

--- resources/views/livewire/pages/landing.blade.php
+++ resources/views/livewire/pages/landing.blade.php
@@ -6,9 +6,9 @@
         <h1 id="visionHubT">VisionHub</h1>
     </div>
     <div id="sloganVH">
-        <h2 id="subTitleVH">Generate the art on your mind!</h2>
+        <h2 id="subTitleVH">{{ __('Generate the art on your mind!') }}</h2>
         <p id="subSloganVH">
-            Select the tool that best suits the idea you want to realize
+            {{ __('Select the tool that best suits the idea you want to realize') }}
         </p>
     </div>
     <div id="mainBodyTemplate">
@@ -18,13 +18,13 @@
                     <div class="flipCardDetails">
                         <div class="flip-card-inner" id="flip-card-inner-community">
                             <div class="flip-card-front" id="flip-card-front-community">
-                                <h2>Great Community!</h2>
+                                <h2>{{ __('Great Community!') }}</h2>
                             </div>
                             <div class="flip-card-back" id="flip-card-back-community">
-                                <h1>Details</h1>
-                                <p>Source of inspiration</p>
-                                <p>View other users' creations</p>
-                                <p>Request for new generation resources</p>
+                                <h1>{{ __('Details') }}</h1>
+                                <p>{{ __('Source of inspiration') }}</p>
+                                <p>{{ __("View other users' creations") }}</p>
+                                <p>{{ __('Request for new generation resources') }}</p>
                             </div>
                         </div>
                     </div>
@@ -33,14 +33,14 @@
                     <div class="flipCardDetails">
                         <div class="flip-card-inner" id="flip-card-inner-upgrades">
                             <div class="flip-card-front" id="flip-card-front-upgrades">
-                                <h2>On future upgrades</h2>
+                                <h2>{{ __('On future upgrades') }}</h2>
                             </div>
                             <div class="flip-card-back" id="flip-card-back-upgrades">
-                                <h1>Details</h1>
-                                <p>New generation resources</p>
-                                <p>Generation of poses</p>
-                                <p>Resource export</p>
-                                <p>Community improvements</p>
+                                <h1>{{ __('Details') }}</h1>
+                                <p>{{ __('New generation resources') }}</p>
+                                <p>{{ __('Generation of poses') }}</p>
+                                <p>{{ __('Resource export') }}</p>
+                                <p>{{ __('Community improvements') }}</p>
                             </div>
                         </div>
                     </div>
@@ -50,18 +50,18 @@
         <div id="wrapper2D" class="genericWrapper">
             <div id="generatordiv2d" class="subVisionHubdiv hidden">
                 <div id="content1">
-                    <h2 class="subVisionHubT" id="2dGeneratorT">2D generator</h2>
-                    <a href="">
-                        <div id="generatorB2d" class="buttonApp">Let's Generate!</div>
+                    <h2 class="subVisionHubT" id="2dGeneratorT">{{ __('2D generator') }}</h2>
+                    <a href="/create">
+                        <div id="generatorB2d" class="buttonApp">{{ __("Let's Generate!") }}</div>
                     </a>
                 </div>
                 <div class="flipCardDetails">
                     <div id="flip-card-inner2d" class="flip-card-inner">
                         <div class="flip-card-front" id="flip-card-front-2d">
-                            <h3>Get more info!</h3>
+                            <h3>{{ __('Get more info!') }}</h3>
                         </div>
                         <div class="flip-card-back" id="flip-card-back-2d">
-                            <h1>Details</h1>
+                            <h1>{{ __('Details') }}</h1>
                             <p>Use of embeddings</p>
                             <p>Multi Loras and Checkpoint on generation</p>
                             <p>Saves your favorites styles to generate</p>
@@ -73,20 +73,20 @@
         <div id="wrapper3D" class="genericWrapper">
             <div id="generatordiv3d" class="subVisionHubdiv hidden">
                 <div id="content1">
-                    <h2 class="subVisionHubT" id="3dGeneratorT">3D generator</h2>
-                    <a href="">
-                        <div id="generatorB3ds" class="buttonApp">Let's Generate!</div>
+                    <h2 class="subVisionHubT" id="3dGeneratorT">{{ __('3D generator') }}</h2>
+                    <a href={{ config('services.angular') . '/generate' }}>
+                        <div id="generatorB3ds" class="buttonApp">{{ __("Let's Generate!") }}</div>
                     </a>
                 </div>
                 <div class="flipCardDetails">
                     <div id="flip-card-inner3d" class="flip-card-inner">
                         <div class="flip-card-front" id="flip-card-front-3d">
-                            <h3>Get more info!</h3>
+                            <h3>{{ __('Get more info!') }}</h3>
                         </div>
                         <div class="flip-card-back" id="flip-card-back-3d">
-                            <h1>Details</h1>
-                            <p>Generation of 3D models from a promp</p>
-                            <p>Get different choices from a generation</p>
+                            <h1>{{ __('Details') }}</h1>
+                            <p>{{ __('Generation of 3D models from a prompt') }}</p>
+                            <p>{{ __('Get different choices from a generation') }}</p>
                         </div>
                     </div>
                 </div>
@@ -101,8 +101,8 @@
                 </div>
                 <div class="gridFooterElement" id="contacts">
                     <p>
-                        <em>This is a Open Source Project <br />
-                            Feel free you propagate your ideas!
+                        <em>{{ __('This is a Open Source Project') }} <br />
+                            {{ __('Feel free you propagate your ideas!') }}
                         </em>
                     </p>
                 </div>
@@ -116,7 +116,7 @@
                     <a href="mailto:inbox.visionhub@gmail.com?subject=New Suggestion to VisionHub">
                         <div id="gitHubLink">
                             <img src="{{ asset('assets/landing/icons8-correo-96.png') }}" alt="" />
-                            Suggestions
+                            {{ __('Suggestions') }}
                         </div>
                     </a>
                 </div>
