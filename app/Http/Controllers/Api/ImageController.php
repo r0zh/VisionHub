6256e6ccafb86133c11484c11b2ad4272b7bfd57
r0zh <r0zh017@proton.me>
2024-06-17 06:17:59 +0000

    refactor: Remove unused methods from ImageController and ThreeDModelController

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -37,32 +37,4 @@ class ImageController extends Controller
         $images = Image::all();
         return response()->json($images);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-    }
 }
--- app/Http/Controllers/Api/ThreeDModelController.php
+++ app/Http/Controllers/Api/ThreeDModelController.php
@@ -78,25 +78,4 @@ class ThreeDModelController extends Controller
         $model->prompt = $request->prompt;
         return $model->save();
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-    }
 }
